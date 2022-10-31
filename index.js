bc20afc46c8e5943bfde0f820a156e0e6d213bc7
Mustafa <mustafamuhith@gmail.com>
2022-10-31 16:21:50 +0000

    first commit with structure for a function

---
+++ index.js
@@ -0,0 +1,29 @@
+const breweryListUl = document.querySelector('.breweries-list')
+const breweryFilter = document.querySelector('#filter-by-type')
+const selectByState = document.querySelector('#select-state-form')
+
+
+function renderBrewery() {
+    // create: li
+
+    // h2
+
+    // div type
+
+    // section with class of 'address' e.g. title, address, postcode
+
+
+    // another section for phone with title and 'n/a'
+
+
+    // link section with class of link
+    // create link with attribute of null and target
+
+    // append link to the section
+
+
+    // append all to the li
+
+
+    // return li
+}
